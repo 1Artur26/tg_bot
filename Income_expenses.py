aa1e747f736fa15befd944d4d2e1c6b25fdce7d9
unknown <slidenkoartur1@gmail.com>
2023-09-13 19:56:55 +0300

    Add main file

--- Income_expenses.py
+++ Income_expenses.py
@@ -2,8 +2,34 @@ from telegram.ext import  CallbackContext
 from telegram import Update, KeyboardButton, ReplyKeyboardMarkup
 from datetime import datetime
 import main
+import json
+import os
 
-# ...
+
+def save_data(data, filename):
+    with open(os.path.join(current_directory, filename), 'w') as file:
+        json.dump(data, file)
+
+
+def load_data(filename):
+    try:
+        with open(os.path.join(current_directory, filename), 'r') as file:
+            data = json.load(file)
+        return data
+    except FileNotFoundError:
+        
+        return {}
+
+CATEGORY = ['Food', 'Sport','Hobby','Family','Home']
+
+current_directory = os.path.dirname(os.path.abspath(__file__))
+
+balance_file_path = os.path.join(current_directory, 'balance.txt')
+
+balance = int(open(balance_file_path, 'r').read())
+expenses_list = load_data('expenses.json')
+incomes_list = load_data('incomes.json')
+
 
 async def Income_expenses_menu(update: Update, context: CallbackContext) -> None:
     await update.callback_query.message.reply_text(
@@ -21,11 +47,6 @@ async def Income_expenses_menu(update: Update, context: CallbackContext) -> None
         "Get category: /get_category \n"
     )
 
-balance = 0
-expenses_list = dict()
-incomes_list = dict()
-CATEGORY = ['Food', 'Sport','Hobby','Family','Home']
-
 
 class Income_Expense:
     def __init__(self, amount: str, category: str, time: datetime = None) :
@@ -37,14 +58,20 @@ class Income_Expense:
         if self.time is not None:
             return f"{self.amount} грн | {self.category} | {self.time.strftime('%Y-%m-%d %H:%M:%S')}"
         return f"{self.amount} грн | {self.category}"
-        
+    
+    def to_dict(self):
+        return {
+            "amount": self.amount,
+            "category": self.category,
+            "time": self.time.strftime('%Y-%m-%d %H:%M:%S') if self.time else None,
+        }
 
 
 async def get_balance(update: Update, context: CallbackContext) -> None:
     await update.message.reply_text(f"Your balance: {balance} грн")    
     
 async def add_expens(update: Update, context: CallbackContext) -> None:
-    user_id = update.message.from_user.id
+    user_id = str(update.message.from_user.id)
     expens_parts = " ".join(context.args).split("|")
     amount = expens_parts[0].strip()
     category = expens_parts[1].strip()
@@ -67,14 +94,16 @@ async def add_expens(update: Update, context: CallbackContext) -> None:
 
     global balance
     balance -= int(amount)
+    open(balance_file_path, 'w').write(str(balance))
     
     expens = Income_Expense(amount, category, time)
-    expenses_list[user_id].append({category: expens})
+    expenses_list[user_id].append({category: expens.to_dict()})
     await update.message.reply_text(f"Expens: {expens} was successfully added!")
+    save_data(expenses_list, 'expenses.json')
 
 
 async def add_income(update: Update, context: CallbackContext) -> None:
-    user_id = update.message.from_user.id
+    user_id = str(update.message.from_user.id)
     income_parts = " ".join(context.args).split("|")
     amount = income_parts[0].strip()
     category = income_parts[1].strip()
@@ -95,17 +124,19 @@ async def add_income(update: Update, context: CallbackContext) -> None:
 
     global balance
     balance += int(amount)
+    open(balance_file_path, 'w').write(str(balance))
 
     income = Income_Expense(amount, category, time)
-    incomes_list[user_id].append({category: income})
+    incomes_list[user_id].append({category: income.to_dict()})
     await update.message.reply_text(f"Income: {income} was successfully added!")
+    save_data(incomes_list, 'incomes.json')
 
 
 async def get_category(update: Update, context: CallbackContext) -> None:
     await update.message.reply_text('\n'.join(CATEGORY))
 
 async def get_expenses(update: Update, context: CallbackContext) -> None:
-    user_id = update.message.from_user.id
+    user_id = str(update.message.from_user.id)
     filters = " ".join(context.args).split("|")
     category_filter = filters[0].strip()
     if len(filters) >= 3:
@@ -139,9 +170,10 @@ async def get_expenses(update: Update, context: CallbackContext) -> None:
         formatted_filtered_expenses = []
         for i, expense in enumerate(user_expenses):
             expense_category = list(expense.keys())[0]
-            expense_date = list(expense.values())[0].time
-            if expense_category == category_filter and expense_date >= datetime.strptime(date_filter, '%Y-%m-%d %H:%M:%S'):
-                formatted_filtered_expenses.append(f"{i + 1}. {list(expense.values())[0]}")
+            expense_date = list(expense.values())[0]['time']
+            expense_data = list(expense.values())[0]
+            if expense_category == category_filter and datetime.strptime(expense_date, '%Y-%m-%d %H:%M:%S') >= datetime.strptime(date_filter, '%Y-%m-%d %H:%M:%S'):
+                formatted_filtered_expenses.append(f"{i + 1}. Category: {expense_data['category']}, Amount: {expense_data['amount']}, Time: {expense_data['time']}")
 
         if formatted_filtered_expenses:
             await update.message.reply_text("\n".join(formatted_filtered_expenses))
@@ -151,7 +183,7 @@ async def get_expenses(update: Update, context: CallbackContext) -> None:
     
 
 async def get_incomes(update: Update, context: CallbackContext) -> None:
-    user_id = update.message.from_user.id
+    user_id = str(update.message.from_user.id)
     filters = " ".join(context.args).split("|")
     category_filter = filters[0].strip()
     if len(filters) >= 3:
@@ -185,7 +217,7 @@ async def get_incomes(update: Update, context: CallbackContext) -> None:
         formatted_filtered_incomes = []
         for i, income in enumerate(user_incomes):
             income_category = list(income.keys())[0]
-            income_date = list(income.values())[0].time
+            income_date = list(income.values())[0]['time']
             if income_category == category_filter and income_date >= datetime.strptime(date_filter, '%Y-%m-%d %H:%M:%S'):
                 formatted_filtered_incomes.append(f"{i + 1}. {list(income.values())[0]}")
 
@@ -196,7 +228,7 @@ async def get_incomes(update: Update, context: CallbackContext) -> None:
         return
 
 async def remove_expense(update: Update, context: CallbackContext) -> None:
-    user_id = update.message.from_user.id
+    user_id = str(update.message.from_user.id)
     try:
         expense_number = int(context.args[0]) - 1
     except (ValueError, IndexError):
@@ -212,16 +244,20 @@ async def remove_expense(update: Update, context: CallbackContext) -> None:
     # Видаляємо витрату
     removed_expense = user_expenses.pop(expense_number)
     category = list(removed_expense.keys())[0]
-    amount = removed_expense[category].amount
+    amount = removed_expense[category]["amount"]
 
     global balance
     balance += int(amount)
 
+    open(balance_file_path, 'w').write(str(balance))
+
     await update.message.reply_text(f"Expense {expense_number + 1} has been removed. Your balance has been updated.")
+
+    save_data(expenses_list, 'expenses.json')
 
 
 async def remove_income(update: Update, context: CallbackContext) -> None:
-    user_id = update.message.from_user.id
+    user_id = str(update.message.from_user.id)
     try:
         income_number = int(context.args[0]) - 1  
     except (ValueError, IndexError):
@@ -237,25 +273,31 @@ async def remove_income(update: Update, context: CallbackContext) -> None:
     # Видаляємо дохід
     removed_income = user_incomes.pop(income_number)
     category = list(removed_income.keys())[0]
-    amount = removed_income[category].amount
+    amount = removed_income[category]["amount"]
 
     global balance
     balance -= int(amount)
 
+    open(balance_file_path, 'w').write(str(balance))
+
     await update.message.reply_text(f"Income {income_number + 1} has been removed. Your balance has been updated.")
 
+    save_data(incomes_list, 'incomes.json')
+
 async def clear_expenses(update: Update, context: CallbackContext) -> None:
-    user_id = update.message.from_user.id
+    user_id = str(update.message.from_user.id)
     if user_id in expenses_list:
-        expenses_list[user_id] = []  # Очищаємо список витрат для користувача
+        expenses_list[user_id] = {}
         await update.message.reply_text("Your expenses list has been cleared.")
+        save_data(expenses_list, 'expenses.json')
     else:
         await update.message.reply_text("You don't have any expenses to clear.")
 
 async def clear_incomes(update: Update, context: CallbackContext) -> None:
-    user_id = update.message.from_user.id
+    user_id = str(update.message.from_user.id)
     if user_id in incomes_list:
-        incomes_list[user_id] = []  # Очищаємо список доходів для користувача
+        incomes_list[user_id] = {}  # Очищаємо список доходів для користувача
         await update.message.reply_text("Your incomes list has been cleared.")
+        save_data(incomes_list, 'income.json')
     else:
         await update.message.reply_text("You don't have any incomes to clear.")
